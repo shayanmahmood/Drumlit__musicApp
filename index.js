e4fb5486da03a5fa6992c0b581fa74c09407b425
shayan__CodeKing <shayanmahmood234@gmail.com>
2022-06-11 22:41:59 +0500

    working ON SOund ....commit __3

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+
 var numberOfDrums = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrums; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        alert("I got Click");
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+     this.style.color = "red";
     });
-   
+
 }
+
+
+// var audio = new Audio('sounds/tom-1.mp3');
+// audio.play();
